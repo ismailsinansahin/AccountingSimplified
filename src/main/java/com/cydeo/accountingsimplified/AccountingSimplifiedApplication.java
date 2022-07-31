a7918fbe078bf188b033bfb89cfb4ed0bb1b8365
Sahin <ismailsinansahin@gmail.com>
2022-07-31 09:54:56 +0300

    Initial commit

---
+++ src/main/java/com/cydeo/accountingsimplified/AccountingSimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.accountingsimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingSimplifiedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingSimplifiedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/accountingsimplified/AccountingSimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.accountingsimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingSimplifiedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
