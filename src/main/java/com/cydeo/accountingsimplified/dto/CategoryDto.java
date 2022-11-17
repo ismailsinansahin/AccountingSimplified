fe849783bcc89cff31077a7a87bb17c031d81d72
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-11-17 14:31:52 +0100

    added profile icon inside header.html, user full name is shown in the profile window. Simple fix for user with Cemil abi

--- src/main/java/com/cydeo/accountingsimplified/dto/CategoryDto.java
+++ src/main/java/com/cydeo/accountingsimplified/dto/CategoryDto.java
@@ -16,8 +16,8 @@ import javax.validation.constraints.Size;
 public class CategoryDto {
 
     private Long id;
-    @NotBlank(message = "Description is a required field")
-    @Size(max = 15, min = 2, message = "Description should have 2-15 characters long")
+    @NotBlank(message = "Description is a required field.")
+    @Size(max = 100, min = 2, message = "Description should have 2-100 characters long.")
     private String description;
 
     private CompanyDto company;
