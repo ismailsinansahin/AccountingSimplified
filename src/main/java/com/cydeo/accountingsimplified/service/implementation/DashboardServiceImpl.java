4b6884906caddda1020db71c31f52760838db593
Baha Tastan <baha@cybertekschool.com>
2022-11-22 21:37:17 +0000

    step 7: consume third party api for currencies

--- src/main/java/com/cydeo/accountingsimplified/service/implementation/DashboardServiceImpl.java
+++ src/main/java/com/cydeo/accountingsimplified/service/implementation/DashboardServiceImpl.java
@@ -1,35 +1,37 @@
 package com.cydeo.accountingsimplified.service.implementation;
 
+import com.cydeo.accountingsimplified.dto.CurrencyApiResponse;
+import com.cydeo.accountingsimplified.dto.CurrencyDto;
 import com.cydeo.accountingsimplified.entity.Company;
 import com.cydeo.accountingsimplified.entity.Invoice;
 import com.cydeo.accountingsimplified.enums.InvoiceStatus;
 import com.cydeo.accountingsimplified.enums.InvoiceType;
 import com.cydeo.accountingsimplified.mapper.MapperUtil;
 import com.cydeo.accountingsimplified.repository.InvoiceRepository;
-import com.cydeo.accountingsimplified.service.CompanyService;
-import com.cydeo.accountingsimplified.service.DashboardService;
-import com.cydeo.accountingsimplified.service.InvoiceProductService;
-import com.cydeo.accountingsimplified.service.SecurityService;
+import com.cydeo.accountingsimplified.service.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class DashboardServiceImpl implements DashboardService {
     private final InvoiceRepository invoiceRepository;
     private final InvoiceProductService invoiceProductService;
     private final MapperUtil mapperUtil;
     private final SecurityService securityService;
 
+    private final CurrencyExchangeClient client;
+
     public DashboardServiceImpl(InvoiceRepository invoiceRepository, InvoiceProductService invoiceProductService,
-                                MapperUtil mapperUtil,SecurityService securityService) {
+                                MapperUtil mapperUtil, SecurityService securityService, CurrencyExchangeClient client) {
         this.invoiceRepository = invoiceRepository;
         this.invoiceProductService = invoiceProductService;
         this.mapperUtil = mapperUtil;
         this.securityService = securityService;
-
+        this.client = client;
     }
 
     @Override
@@ -41,11 +43,10 @@ public class DashboardServiceImpl implements DashboardService {
         Company company = mapperUtil.convert(securityService.getLoggedInUser().getCompany(), new Company());
         List<Invoice> allApprovedInvoicesOfCompany = invoiceRepository
                 .findInvoicesByCompanyAndInvoiceStatus(company, InvoiceStatus.APPROVED);
-        for(Invoice invoice : allApprovedInvoicesOfCompany){
-            if(invoice.getInvoiceType() == InvoiceType.PURCHASE){
+        for (Invoice invoice : allApprovedInvoicesOfCompany) {
+            if (invoice.getInvoiceType() == InvoiceType.PURCHASE) {
                 totalCost += invoiceProductService.getTotalOfInvoiceProduct(invoice.getId());
-            }
-            else{
+            } else {
                 totalSales += invoiceProductService.getTotalOfInvoiceProduct(invoice.getId());
                 profitLoss += invoiceProductService.getProfitLossOfInvoiceProduct(invoice.getId());
             }
@@ -58,8 +59,19 @@ public class DashboardServiceImpl implements DashboardService {
 
 
     @Override
-    public List<String> getExchangeRates() {
-        return null;
+    public CurrencyDto getExchangeRates() {
+        CurrencyApiResponse currency = client.getUsdBasedCurrencies();
+        CurrencyDto currencyDto= CurrencyDto.builder()
+                .euro(currency.getUsd().getEur())
+                .britishPound(currency.getUsd().getGbp())
+                .indianRupee(currency.getUsd().getInr())
+                .japaneseYen(currency.getUsd().getJpy())
+                .build();
+
+        log.info("Currencies are fetched for the date : "+currency.getDate());
+
+        return currencyDto;
+
     }
 
 
