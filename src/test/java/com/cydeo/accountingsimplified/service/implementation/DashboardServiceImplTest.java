76a9ada1e49fa315304f7447ecf2c38c98bc0e0f
Baha Tastan <baha@cybertekschool.com>
2022-11-25 20:05:36 +0000

    step 7: currencies cache enabled

--- src/test/java/com/cydeo/accountingsimplified/service/implementation/DashboardServiceImplTest.java
+++ src/test/java/com/cydeo/accountingsimplified/service/implementation/DashboardServiceImplTest.java
@@ -1,19 +1,17 @@
 package com.cydeo.accountingsimplified.service.implementation;
 
-import com.cydeo.accountingsimplified.dto.CurrencyApiResponse;
+
 import com.cydeo.accountingsimplified.dto.CurrencyDto;
 import com.cydeo.accountingsimplified.service.DashboardService;
-
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 
 @SpringBootTest
@@ -32,13 +30,9 @@ class DashboardServiceImplTest {
         assertThat(response).isNotNull();
         assertThat(response).isInstanceOf(CurrencyDto.class);
         assertThat(response)
-                .extracting("britishPound","euro","canadianDollar","indianRupee")
+                .extracting("britishPound", "euro", "canadianDollar", "indianRupee")
                 .hasSizeGreaterThan(0);
 
 
-
-
     }
-
-
 }
