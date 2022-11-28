81134d429dcec36655b61b56d3584f95f5dc1d59
owermaiden <ower.maiden@cybertekschool.com>
2022-11-29 01:20:25 +0300

    Final Touch

--- src/main/java/com/cydeo/accountingsimplified/controller/DashboardController.java
+++ src/main/java/com/cydeo/accountingsimplified/controller/DashboardController.java
@@ -1,11 +1,18 @@
 package com.cydeo.accountingsimplified.controller;
 
+import com.cydeo.accountingsimplified.dto.InvoiceDto;
+import com.cydeo.accountingsimplified.entity.Invoice;
+import com.cydeo.accountingsimplified.enums.InvoiceStatus;
+import com.cydeo.accountingsimplified.enums.InvoiceType;
 import com.cydeo.accountingsimplified.service.CompanyService;
 import com.cydeo.accountingsimplified.service.DashboardService;
 import com.cydeo.accountingsimplified.service.InvoiceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Controller
 public class DashboardController {
@@ -26,7 +33,42 @@ public class DashboardController {
         model.addAttribute("invoices", invoiceService.getLastThreeInvoices());
         model.addAttribute("exchangeRates", dashboardService.getExchangeRates());
         model.addAttribute("title", "Cydeo Accounting-Dashboard");
+        model.addAttribute("chartData", getChartData(InvoiceType.PURCHASE));
+        model.addAttribute("chartData2", getChartData(InvoiceType.SALES));
+        model.addAttribute("chartData3", getProfitLoss());
         return "dashboard";
+    }
+
+    private List<List<Object>> getChartData(InvoiceType invoiceType) throws Exception {
+        List<InvoiceDto> invoiceDtos = invoiceService.getAllInvoicesOfCompany(invoiceType);
+        var totalPrices = invoiceDtos.stream()
+                .map(InvoiceDto::getPrice)
+                .reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+        var totalTax = invoiceDtos.stream()
+                .map(InvoiceDto::getTax)
+                .reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+        return List.of(
+                List.of("Products", totalPrices),
+                List.of("Tax", totalTax)
+        );
+    }
+
+    private List<List<Object>> getProfitLoss(){
+        List<InvoiceDto> invoiceDtos = invoiceService.getAllInvoicesByInvoiceStatus(InvoiceStatus.APPROVED);
+        var profit = invoiceDtos.stream()
+                .map(inv -> invoiceService.getProfitLossOfInvoice(inv.getId()))
+                .filter(value -> value.compareTo(BigDecimal.ZERO) > 0)
+                .reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+        var loss = invoiceDtos.stream()
+                .map(inv -> invoiceService.getProfitLossOfInvoice(inv.getId()))
+                .filter(value -> value.compareTo(BigDecimal.ZERO) <  0)
+                .map(value -> value.abs())
+                .reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+
+        return List.of(
+                List.of("Profit", profit),
+                List.of("Loss", loss)
+        );
     }
 
 }
