e075decb8feeebc18ef1526f869c17c68c28dd15
Cemil <chrisw@cydeo.com>
2022-12-18 10:40:08 +0300

    added explanation for loggging aspect methods

--- src/main/java/com/cydeo/accountingsimplified/aspect/LoggingAspect.java
+++ src/main/java/com/cydeo/accountingsimplified/aspect/LoggingAspect.java
@@ -16,11 +16,16 @@ public class LoggingAspect {
 
     private final SecurityService securityService;
 
+    // catch if CompanyService activate() or deactivate() method is executed
+    // (..) : method parameters are not important
     @Pointcut("execution(* com.cydeo.accountingsimplified.service.CompanyService.activate(..)) " +
             "|| execution(* com.cydeo.accountingsimplified.service.CompanyService.deactivate(..))")
     public void anyCompanyActivationDeactivation() {
     }
 
+    // if anyCompanyActivationDeactivation() method (above) is executed,
+    // log the method name (activate / deactivate), company name of the activated / deactivated,
+    // firstname, lastname and username of the logged-in user after that method executed
     @AfterReturning(value = "anyCompanyActivationDeactivation()", returning = "dto")
     public void afterAnyCompanyActivation(JoinPoint joinPoint, CompanyDto dto) {
         log.info("Method: {}, Company Name:{}, User -> FirstName: {}, LastName: {}, Username: {}",
