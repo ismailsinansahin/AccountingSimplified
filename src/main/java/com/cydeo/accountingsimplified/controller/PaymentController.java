a711404f121abeed7a031f665ac775bed25edf6b
owermaiden <ower.maiden@cybertekschool.com>
2023-01-01 21:27:41 +0100

    Exception Added

--- src/main/java/com/cydeo/accountingsimplified/controller/PaymentController.java
+++ src/main/java/com/cydeo/accountingsimplified/controller/PaymentController.java
@@ -2,11 +2,9 @@ package com.cydeo.accountingsimplified.controller;
 
 import com.cydeo.accountingsimplified.dto.PaymentDto;
 import com.cydeo.accountingsimplified.entity.common.ChargeRequest;
-import com.cydeo.accountingsimplified.service.CompanyService;
 import com.cydeo.accountingsimplified.service.PaymentService;
 import com.cydeo.accountingsimplified.service.implementation.StripeServiceImpl;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Charge;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,17 +21,15 @@ public class PaymentController {
 
     private final StripeServiceImpl stripeServiceImpl;
     private final PaymentService paymentService;
-    private final CompanyService companyService;
 
-    public PaymentController(StripeServiceImpl stripeServiceImpl, PaymentService paymentService, CompanyService companyService) {
+    public PaymentController(StripeServiceImpl stripeServiceImpl, PaymentService paymentService) {
         this.stripeServiceImpl = stripeServiceImpl;
         this.paymentService = paymentService;
-        this.companyService = companyService;
     }
 
 
     @GetMapping({"/list", "/list/{year}"})
-    public String createPayment(@RequestParam(value = "year", required = false) String selectedYear, Model model) {
+    public String list(@RequestParam(value = "year", required = false) String selectedYear, Model model) {
 
         int selectedYear1 = (selectedYear == null || selectedYear.isEmpty()) ? LocalDate.now().getYear() : Integer.parseInt(selectedYear);
         paymentService.createPaymentsIfNotExist(selectedYear1);
@@ -55,7 +51,6 @@ public class PaymentController {
         return "payment/payment-method";
     }
 
-    // charge controller
 
     @PostMapping("/charge/{id}")
     public String charge(ChargeRequest chargeRequest, @PathVariable("id") Long id, Model model)
@@ -67,24 +62,6 @@ public class PaymentController {
         return "redirect:/payments/list";
     }
 
-    @ExceptionHandler(StripeException.class)
-    public String handleError(Model model, StripeException ex) {
-        model.addAttribute("error", ex.getMessage());
-        return "payment/payment-success";
-    }
-
-    // To invoice
-
-    @GetMapping("/toInvoice/{id}")
-    public String toInvoice(@PathVariable("id") Long id, Model model) {
-
-        model.addAttribute("payment", paymentService.getPaymentById(id));
-        model.addAttribute("company", companyService.getCompanyByLoggedInUser());
-
-        return "payment/payment-success";
-    }
-
-    // charge controller
 
 
 
