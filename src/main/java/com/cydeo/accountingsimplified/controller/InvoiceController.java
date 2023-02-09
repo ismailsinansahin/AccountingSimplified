b5d7a854b02208c7f1065957036c22cafcd954dc
owermaiden <ower.maiden@cybertekschool.com>
2023-02-09 19:39:16 +0100

    invoice controller

---
+++ src/main/java/com/cydeo/accountingsimplified/controller/InvoiceController.java
@@ -0,0 +1,71 @@
+package com.cydeo.accountingsimplified.controller;
+
+import com.cydeo.accountingsimplified.dto.InvoiceDto;
+import com.cydeo.accountingsimplified.dto.ResponseWrapper;
+import com.cydeo.accountingsimplified.enums.InvoiceType;
+import com.cydeo.accountingsimplified.service.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/invoices")
+public class InvoiceController {
+
+    private final InvoiceService invoiceService;
+
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getInvoices(InvoiceType invoiceType) throws Exception {
+        var invoices = invoiceService.getAllInvoicesOfCompany(invoiceType);
+        return ResponseEntity.ok(new ResponseWrapper("Invoices successfully retrieved",invoices, HttpStatus.OK));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseWrapper> getInvoiceByTypeAndId(@PathVariable("id") Long id) throws Exception {
+       var invoice = invoiceService.findInvoiceById(id);
+       return ResponseEntity.ok(new ResponseWrapper("Invoice successfully retrieved",invoice, HttpStatus.OK));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseWrapper> create(@RequestBody InvoiceDto invoiceDto, InvoiceType invoiceType) {
+        invoiceService.save(invoiceDto, invoiceType);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Invoice successfully created",HttpStatus.CREATED));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseWrapper> update(@PathVariable("id") Long id, @RequestBody InvoiceDto invoiceDto) {
+        invoiceService.update(id, invoiceDto);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseWrapper("Invoice successfully updated",HttpStatus.OK));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseWrapper> delete(@PathVariable("id") Long id){
+        invoiceService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseWrapper("Invoice successfully deleted",HttpStatus.OK));
+    }
+
+    @GetMapping("/approve/{id}")
+    public ResponseEntity<ResponseWrapper> approve(@PathVariable("id") Long id){
+        invoiceService.approve(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseWrapper("Invoice approved",HttpStatus.OK));
+    }
+
+//    @PostMapping("/invoice-product/{id}")
+//    public ResponseEntity<ResponseWrapper> addInvoiceProduct(@PathVariable("id") Long id, @RequestBody InvoiceProductDto invoiceProductDto) {
+//        invoiceProductService.save(id, invoiceProductDto);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("InvoiceProduct successfully created",HttpStatus.CREATED));
+//    }
+//
+//    @GetMapping("/removeInvoiceProduct/{invoiceId}/{invoiceProductId}")
+//    public String removeInvoiceProduct(@PathVariable("invoiceId") Long invoiceId,
+//                                       @PathVariable("invoiceProductId") Long invoiceProductId) {
+//        invoiceProductService.delete(invoiceProductId);
+//        return "redirect:/purchaseInvoices/update/" + invoiceId;
+//    }
+
+
+}
